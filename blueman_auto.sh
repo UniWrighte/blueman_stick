162977bba814d5f848f98cbcc19b7847370da07c
David Kamer <david.kamer@uniwrighte.com>
2018-12-11 14:57:19 -0500

    Added executable bit to shell files, added script to reset autostart.

---
+++ blueman_auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mv ~/blueman.desktop.bak /etc/xdg/autostart/blueman.desktop
+
+echo "blueman has been added to autostart."
+
+#todo - add check to toggle and echo correct results
