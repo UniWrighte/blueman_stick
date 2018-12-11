8c0602b1c9ac855d0b48cfd93eed152a5ad4954c
David Kamer <david.kamer@uniwrighte.com>
2018-12-11 01:40:47 -0500

    remove autostart

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blueman_stick
 Toggle blueman bluetooth manager's autostart. 
+
+No current feature to turn autostart back on. Adding soon.
---
+++ blueman_stick.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mv /etc/xdg/autostart/blueman.desktop ~/blueman.desktop.bak
+
+echo "blueman has been removed from autostart."
+
+#todo - add check to toggle and echo correct results
